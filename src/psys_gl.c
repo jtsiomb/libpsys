222beb0d8aec4dca080979a6b1b2bf18edb50ae3
John Tsiombikas <nuclear@member.fsf.org>
2012-09-18 09:38:48 +0300

    the billboarding was wrong ...

--- src/psys_gl.c
+++ src/psys_gl.c
@@ -18,17 +18,6 @@
 
 void psys_gl_draw_start(struct psys_emitter *em, void *cls)
 {
-	float xform[16];
-
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-
-	glGetFloatv(GL_MODELVIEW_MATRIX, xform);
-	xform[0] = xform[5] = xform[10] = 1.0;
-	xform[1] = xform[2] = xform[4] = xform[6] = xform[8] = xform[9] = 0.0;
-
-	glLoadMatrixf(xform);
-
 	glPushAttrib(GL_ENABLE_BIT);
 	glDisable(GL_LIGHTING);
 
@@ -41,39 +30,48 @@ void psys_gl_draw_start(struct psys_emitter *em, void *cls)
 	}
 
 	glDepthMask(0);
-
-	glBegin(GL_QUADS);
-	glColor3f(1, 1, 1);
 }
 
 void psys_gl_draw(struct psys_emitter *em, struct psys_particle *p, void *cls)
 {
 	float hsz = p->size / 2.0;
 
+	float xform[16];
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+
+	glTranslatef(p->pos.x, p->pos.y, p->pos.z);
+
+	glGetFloatv(GL_MODELVIEW_MATRIX, xform);
+	xform[0] = xform[5] = xform[10] = 1.0;
+	xform[1] = xform[2] = xform[4] = xform[6] = xform[8] = xform[9] = 0.0;
+	glLoadMatrixf(xform);
+
+	glBegin(GL_QUADS);
 	glColor4f(p->color.x, p->color.y, p->color.z, p->alpha);
 
 	glTexCoord2f(0, 0);
-	glVertex3f(p->pos.x - hsz, p->pos.y - hsz, p->pos.z);
+	glVertex2f(-hsz, -hsz);
 
 	glTexCoord2f(1, 0);
-	glVertex3f(p->pos.x + hsz, p->pos.y - hsz, p->pos.z);
+	glVertex2f(hsz, -hsz);
 
 	glTexCoord2f(1, 1);
-	glVertex3f(p->pos.x + hsz, p->pos.y + hsz, p->pos.z);
+	glVertex2f(hsz, hsz);
 
 	glTexCoord2f(0, 1);
-	glVertex3f(p->pos.x - hsz, p->pos.y + hsz, p->pos.z);
+	glVertex2f(-hsz, hsz);
+	glEnd();
+
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
 }
 
 void psys_gl_draw_end(struct psys_emitter *em, void *cls)
 {
-	glEnd();
-
 	glDepthMask(1);
 	glPopAttrib();
-
-	glMatrixMode(GL_MODELVIEW);
-	glPopMatrix();
 }
 
 
